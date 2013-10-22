f9a6cab28c847df9c55e187dc859877367cf2a35
Joakim Lundin <joakim.lundin@su.se>
2013-10-22 09:31:04 +0200

    Added correct groupId

--- RoleAccessManagerGrailsPlugin.groovy
+++ RoleAccessManagerGrailsPlugin.groovy
@@ -4,6 +4,7 @@ import org.codehaus.groovy.grails.plugins.web.filters.FiltersConfigArtefactHandl
 class RoleAccessManagerGrailsPlugin {
   // the plugin version
   def version = "0.2.7"
+  def groupId = "se.su.it.grails.plugins"
   // the version or versions of Grails the plugin is designed for
   def grailsVersion = "2.0 > *"
   // the other plugins this plugin depends on
