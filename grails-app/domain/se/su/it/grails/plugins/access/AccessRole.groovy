a2a16bd78b7382ab0bdb34589ffb22ccb468d717
Tommy Andersson <tommy.andersson@su.se>
2013-11-06 13:29:46 +0100

    DEVIDM-118: Just working on role and scope now instead of manually supplying a uri.

--- grails-app/domain/se/su/it/grails/plugins/access/AccessRole.groovy
+++ grails-app/domain/se/su/it/grails/plugins/access/AccessRole.groovy
@@ -5,10 +5,10 @@ import groovy.util.logging.Slf4j
 @Slf4j
 class AccessRole {
 
-  String base
   String displayName
-  String env
   String uri
+
+  private final static String BASE = "urn:mace:swami.se:gmai:"
 
   static mapping = {
     cache true
@@ -16,10 +16,42 @@ class AccessRole {
   }
 
   static constraints = {
-    base(nullable: false, blank: false, unique:'env')
     displayName(nullable: false, blank: false)
-    env(blank: false)
     uri(nullable: false, blank: false, unique: true)
+  }
+
+  public static AccessRole createOrUpdateInstance(String displayName, String role, Map scope) {
+
+    if (!displayName) {
+      throw new IllegalArgumentException("Display name is invalid '$displayName'")
+    }
+
+    String uri = composeUri(role, scope)
+
+    withTransaction { status ->
+      try {
+        findOrSaveWhere([displayName: displayName, uri: uri])
+      } catch (ex) {
+        log.error "Failed to create/update RoleAccess for uri: $uri", ex
+        status.setRollbackOnly()
+      }
+    }
+  }
+
+  private static String composeUri(String role, Map scope) {
+    if (!role) {
+      throw new IllegalArgumentException("System name is invalid '$role'")
+    }
+
+    StringBuilder sb = new StringBuilder()
+
+    sb.append(BASE + role)
+
+    scope?.each { k, v ->
+      sb.append(":$k=$v")
+    }
+
+    return sb.toString()
   }
 
   @Override
@@ -33,82 +65,13 @@ class AccessRole {
   @Override
   boolean equals(Object o) {
     if (!o instanceof AccessRole) {
-      return super.equals()
+      return super.equals(o)
     }
 
     return (this.uri == (o as AccessRole).uri)
   }
 
-  public static String getBaseFromUri(String uri) {
-    String base = null
-
-    if (!uri) { return null }
-
-    try {
-      /** TODO: Sadly implemented regex, should find a better solution, but this will do for now. */
-      def matcher = (uri =~ /(urn:mace:swami.se:gmai):(.*?):(.*?)(:.*?)?$/)
-        if (matcher?.size() != 1) {
-          log.error "Unable to extract base from gmai => $uri"
-        } else {
-          base = matcher[0][1,2,3].join(':')
-        }
-    } catch (ex) {
-      log.error "Failed to parse base from uri: $uri", ex
-    }
-
-    return base
-  }
-
-  public static String getEnvFromUri(String uri) {
-    String env = null
-
-    if (!uri) { return null }
-
-    try {
-      def matcher = (uri =~ /env=(\w+)/)
-      if (matcher?.size() != 1) {
-        log.error "Unable to extract environment from gmai => $uri"
-      } else {
-        env = matcher[0][1]
-      }
-    } catch (ex) {
-      log.error "Failed to parse env from uri: $uri", ex
-    }
-
-    return env
-  }
-
-  public static AccessRole createOrUpdateInstance(String displayName, String uri) {
-    AccessRole accessRole = null
-
-    String base = getBaseFromUri(uri)
-    String env = getEnvFromUri(uri)
-
-    accessRole = findByBaseAndEnv(base, env)
-
-    withTransaction { status ->
-      try {
-        if (accessRole) {
-          accessRole.displayName = displayName
-          accessRole.uri = uri
-        } else {
-          accessRole = new AccessRole(displayName: displayName, uri: uri)
-        }
-        accessRole.save(flush:true, failOnError: true)
-      } catch (ex) {
-        log.error "Failed to create/update RoleAccess for uri: $uri", ex
-        status.setRollbackOnly()
-      }
-    }
-
-  }
-
-  transient beforeValidate() {
-    this.base = getBaseFromUri(this.uri)
-    this.env = getEnvFromUri(this.uri)
-  }
-
   String toString() {
-    return "displayName => $displayName,  uri => $uri,  env => $env"
+    return "displayName => $displayName,  uri => $uri"
   }
 }
